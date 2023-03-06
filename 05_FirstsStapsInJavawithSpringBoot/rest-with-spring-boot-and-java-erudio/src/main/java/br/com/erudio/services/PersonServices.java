71528b285b1d0573d73250f4894577323570dc52
Paulo Sérgio Ornelas Cardoso <paulo.ornelas.cardoso@gmail.com>
2023-03-06 17:49:51 -0300

    Implementando os verbos GET, POST, PUT e DELETE

---
+++ 05_FirstsStapsInJavawithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/services/PersonServices.java
@@ -0,0 +1,74 @@
+package br.com.erudio.services;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.logging.Logger;
+
+import org.springframework.stereotype.Service;
+
+import br.com.erudio.model.Person;
+
+@Service
+public class PersonServices {
+
+	private static final AtomicLong counter = new AtomicLong();
+	private Logger logger = Logger.getLogger(PersonServices.class.getName());
+
+	public Person findById(String id) {
+
+		logger.info("Finding one person!");
+
+		Person person = new Person();
+
+		person.setId(counter.incrementAndGet());
+		person.setFirstName("Paulo");
+		person.setLestName("Ornelas");
+		person.setAddress("Guarulhos - São Paulo - Brasil");
+		person.setGender("Male");
+
+		return person;
+	}
+
+	public List<Person> findAll() {
+
+		logger.info("Finding all people!");
+
+		List<Person> persons = new ArrayList<Person>();
+
+		for (int i = 0; i < 8; i++) {
+			Person person = mockPerson(i);
+			persons.add(person);
+		}
+
+		return persons;
+	}
+
+	public Person create(Person person) {
+		logger.info("Creating one person!");
+		return person;
+	}
+	
+	public Person update(Person person) {
+		logger.info("Updating one person!");
+		return person;
+	}
+	
+	public void delete(String id) {
+		logger.info("Deleting one person!");
+	}
+
+	private Person mockPerson(int i) {
+		Person person = new Person();
+
+		person.setId(counter.incrementAndGet());
+		person.setFirstName("Person name " + i);
+		person.setLestName("Lest name" + i);
+		person.setAddress("Some address in Brasil " + i);
+		person.setGender("Male");
+
+		return person;
+	}
+
+}
