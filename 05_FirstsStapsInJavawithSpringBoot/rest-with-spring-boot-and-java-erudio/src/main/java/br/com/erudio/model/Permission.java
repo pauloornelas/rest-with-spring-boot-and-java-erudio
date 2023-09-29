1eda9d857ef30fae437d927ad84f07f24e2586be
Paulo Sérgio Ornelas Cardoso <paulo.ornelas.cardoso@gmail.com>
2023-09-29 19:29:32 -0300

    Inserindo security

--- 05_FirstsStapsInJavawithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/model/Permission.java
+++ 05_FirstsStapsInJavawithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/model/Permission.java
@@ -1,7 +1,6 @@
 package br.com.erudio.model;
 
 import java.io.Serializable;
-import java.util.Objects;
 
 import org.springframework.security.core.GrantedAuthority;
 
@@ -22,12 +21,13 @@ public class Permission implements GrantedAuthority, Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@Column(name = "description", length = 255)
+	@Column
 	private String description;
 
-	public Permission(Long id, String description) {
+	public Permission() {
 	}
 
+	@Override
 	public String getAuthority() {
 		return this.description;
 	}
@@ -50,7 +50,11 @@ public class Permission implements GrantedAuthority, Serializable {
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(description, id);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
 
 	@Override
@@ -62,6 +66,16 @@ public class Permission implements GrantedAuthority, Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		Permission other = (Permission) obj;
-		return Objects.equals(description, other.description) && Objects.equals(id, other.id);
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
 }
--- 05_FirstsStapsInJavawithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/repositories/UserRepository.java
+++ 05_FirstsStapsInJavawithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/repositories/UserRepository.java
@@ -10,6 +10,6 @@ import br.com.erudio.model.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-	@Query("SELECT u FROM User WHERE u.userName =:userName")
+	@Query("SELECT u FROM User u WHERE u.userName =:userName")
 	User findByUserName(@Param("userName") String userName);
 }
