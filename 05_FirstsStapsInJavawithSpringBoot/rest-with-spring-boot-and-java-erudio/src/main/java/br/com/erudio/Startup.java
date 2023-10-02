e923a32760435706c68e5b01c073a1919ee48866
Paulo Sérgio Ornelas Cardoso <paulo.ornelas.cardoso@gmail.com>
2023-10-02 16:29:18 -0300

    Ajuste nos usuarios

--- 05_FirstsStapsInJavawithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/Startup.java
+++ 05_FirstsStapsInJavawithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/Startup.java
@@ -1,14 +1,14 @@
 package br.com.erudio;
 
-import java.util.HashMap;
-import java.util.Map;
+//import java.util.HashMap;
+//import java.util.Map;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
-import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder.SecretKeyFactoryAlgorithm;
+//import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+//import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder.SecretKeyFactoryAlgorithm;
 
 @SpringBootApplication
 public class Startup {
@@ -16,7 +16,7 @@ public class Startup {
 	public static void main(String[] args) {
 		SpringApplication.run(Startup.class, args);
 
-		Map<String, PasswordEncoder> encoders = new HashMap<>();
+		/*Map<String, PasswordEncoder> encoders = new HashMap<>();
 
 		encoders.put("pbkdf2", new Pbkdf2PasswordEncoder("", 8, 185000, SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA256));
 
@@ -24,8 +24,9 @@ public class Startup {
 
 		passwordEncoder.setDefaultPasswordEncoderForMatches(new Pbkdf2PasswordEncoder("", 8, 185000, SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA256));
 
-		String result = passwordEncoder.encode("admin123");
-
-		System.out.println("My hash " + result);
+		String result1 = passwordEncoder.encode("admin123");
+		String result2 = passwordEncoder.encode("admin234");
+		System.out.println("My hash result1 " + result1);
+		System.out.println("My hash result2 " + result2);*/
 	}
 }
--- 05_FirstsStapsInJavawithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/resources/db/migration/V8__Insert_Data_In_Users.sql
+++ 05_FirstsStapsInJavawithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/resources/db/migration/V8__Insert_Data_In_Users.sql
@@ -1,3 +1,3 @@
 ﻿INSERT INTO `users` (`user_name`, `full_name`, `password`, `account_non_expired`, `account_non_locked`, `credentials_non_expired`, `enabled`) VALUES
-	('leandro', 'Leandro Costa', '19bbf735b27066f2f145e602624e1b24a3fbc54cd5dfd3143fc5feea6bdee9e139ca7332d4806b9f', b'1', b'1', b'1', b'1'),
-	('flavio', 'Flavio Costa', '75ec349c1b0ef4ee7b249d0b83ae4861853f3aa77bce8c4b15f28cd43c6424ab4f29df431831bb0d', b'1', b'1', b'1', b'1');
+	('leandro', 'Leandro Costa', 'fc9464057cdecdcdfb57b4c8bc3969efc193626d762f46a2c69910339d378aa9772160ca9b35d4a8', b'1', b'1', b'1', b'1'),
+	('flavio', 'Flavio Costa', '4fbecdaecb5fb29233f49fe6e89df19a5eb87be7053a0e62af78cec1a94e973eff43d0c21d754867', b'1', b'1', b'1', b'1');
